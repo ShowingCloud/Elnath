a80a9a74dbfec071fef1f3c2c0f5481d880b4340
chenjia <409388911@qq.com>
2014-08-14 12:25:02 +0000

    Modified css of product page

--- vendor/assets/javascripts/spree/frontend/vertical_scroll.js
+++ vendor/assets/javascripts/spree/frontend/vertical_scroll.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
 	$("#product-thumbnails").attr("class", "thumbnails jcarousel jcarousel-skin-tango");
-    $("#product-thumbnails li img").attr("height", "112px");
+    $("#product-thumbnails li img").attr("height", "110px");
     $("#product-thumbnails li img").attr("width", "105px");
     jQuery('#product-thumbnails').jcarousel({
         vertical: true,
