31900f77e9194f8e02b7bf5a6fa9aa93818f5697
WANG Guoqin <wgq@showingcloud.com>
2014-09-21 14:04:22 +0000

    load all products on index page

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,6 +1,7 @@
 module Spree
 	ProductsController.class_eval do
 		before_action :write_recently_visited, only: :show
+		before_action :load_all_products, only: :index
 
 		def favorites
 			@favorites = Product.active(current_currency).where(:id => session[:favorite])
@@ -22,6 +23,14 @@ module Spree
 		def	write_recently_visited
 			load_product
 			session[:recently_visited] = Array(session[:recently_visited]).insert(0, @product.id).uniq.take(10)
+		end
+
+		def load_all_products
+			if try_spree_current_user.try(:has_spree_role?, "admin")
+				@all_products = Product.with_deleted
+			else
+				@all_products = Product.active(current_currency)
+			end
 		end
 
 	end
