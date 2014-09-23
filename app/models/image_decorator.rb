aca461d35b9c677bc7599b129786f1a9a12aceaf
chenjia <409388911@qq.com>
2014-09-23 16:25:27 +0000

    Modified image pixel

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,8 +1,8 @@
 Spree::Image.class_eval do
 attachment_definitions[:attachment][:styles] = {
   :mini => '48x48>', # thumbs under image
-  :small => '105x112>', # images on category view
-  :product => '350x500>', # full product image
-  :large => '700x1000>' # light box image
+  :small => '112x112>', # images on category view
+  :product => '500x500>', # full product image
+  :large => '1000x1000>' # light box image
 }
  end
