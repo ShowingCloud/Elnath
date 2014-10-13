d4bd8ea96781af407c13693cc32f3f9d06081b1f
chenjia <409388911@qq.com>
2014-10-13 19:09:56 +0000

    modified my favorite products page

--- vendor/assets/javascripts/spree/frontend/21.ajax.js
+++ vendor/assets/javascripts/spree/frontend/21.ajax.js
@@ -11,6 +11,30 @@ jQuery(document).ready(function(){
 		});
 		alert('加入收藏夹成功！');
 	});
+	$('.addToFavouriteBtn').click(function(){
+		var productId=$(this).parent().find('productId').val();
+		$.ajax ({
+			url:	"/add_favorite",
+			type:	"get",
+			dataType:	"json",
+			data:{
+					product_id:productId
+			}
+		});
+		alert('加入收藏夹成功！');
+	});
+	$('.deleteFavorite').click(function(){
+		var productId=$(this).attr('productid');
+		$.ajax ({
+			url:	"/remove_favorite",
+			type:	"get",
+			dataType:	"json",
+			data:{
+					product_id:productId
+			}
+		});
+		alert('shanchu收藏夹成功！');
+	});
 	$('.delete_addr').click(function(){
 		var addrId = $(this).attr('addr_id');
 		$.ajax ({
